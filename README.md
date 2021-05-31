d981021c2b178a8bfe707904323962faa22dd55e
Hữu Ðà Trần <huuda.tran@gmail.com>
2021-05-31 14:34:21 -0400

    - Adding service-worker update example

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Repo just to learn PWA.
 
 See: https://www.freecodecamp.org/news/build-a-pwa-from-scratch-with-html-css-and-javascript/
+
+See: https://developers.google.com/web/fundamentals/primers/service-workers
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -24,6 +24,7 @@ self.addEventListener("install", installEvent => {
 })
 
 self.addEventListener("fetch", fetchEvent => {
+  console.log('service-worker fetch:', fetchEvent.request);
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then(res => {
       return res || fetch(fetchEvent.request)
@@ -31,3 +32,18 @@ self.addEventListener("fetch", fetchEvent => {
   )
 })
 
+// This will be called when the service worker is updated. Imagine we want new
+// caches and delete any old caches that are not matching the new names.
+self.addEventListener('activate', event => {
+  console.log('service-worker updated. Calling "activate" event.');
+
+  var cacheAllowList = [ 'pages-cache-v1', 'blog-posts-cache-v1' ];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => Promise.all(cacheNames.map(cacheName => {
+      if (cacheAllowList.indexOf(cacheName) === -1) {
+        return caches.delete(cacheName);
+      }
+    })));
+  );
+});
